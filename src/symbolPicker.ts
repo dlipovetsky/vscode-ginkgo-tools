ba1288816059c5036cd4b91c655086f95db73cbd
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2021-01-23 00:05:56 -0800

    symbolPicker: Use revealRange and setDecoration
    Signed-off-by: Daniel Lipovetsky <dlipovetsky@d2iq.com>

--- src/symbolPicker.ts
+++ src/symbolPicker.ts
@@ -1,6 +1,18 @@
 import * as vscode from 'vscode';
 import * as outliner from './outliner';
 
+const symbolHighlightDecorationType = vscode.window.createTextEditorDecorationType({
+    light: {
+        backgroundColor:  { id: 'editor.selectionHighlightBackground' },
+    },
+    dark: {
+        backgroundColor:  { id: 'editor.selectionHighlightBackground' },
+		borderWidth: '1px',
+		borderStyle: 'solid',
+        borderColor: { id: 'contrastActiveBorder' }
+    },
+});
+
 class GinkgoNodeItem implements vscode.QuickPickItem {
     label: string;
     description: string;
@@ -27,20 +39,25 @@ export async function fromTextEditor(editor: vscode.TextEditor) {
             if (!selection[0]) {
                 return;
             }
-            const anchor = editor.document.positionAt(selection[0].node.start);
-            const active =  editor.document.positionAt(selection[0].node.end);
-            editor.selection = new vscode.Selection(anchor, active);
+            const start = editor.document.positionAt(selection[0].node.start);
+            const end = editor.document.positionAt(selection[0].node.end);
+            const range = new vscode.Range(start, end);
+            editor.revealRange(range);
+            editor.setDecorations(symbolHighlightDecorationType, [range]);
         });
         picker.onDidAccept(() => {
-            if(!picker.selectedItems[0]) {
+            if (!picker.selectedItems[0]) {
                 return;
             }
             const anchor = editor.document.positionAt(picker.selectedItems[0].node.start);
             editor.selection = new vscode.Selection(anchor, anchor);
-
+            editor.setDecorations(symbolHighlightDecorationType, []);
             picker.dispose();
         });
-        picker.onDidHide(() => picker.dispose());
+        picker.onDidHide(() => {
+            editor.setDecorations(symbolHighlightDecorationType, []);
+            picker.dispose();
+        });
         picker.show();
     } finally {
         // handle rejected promise
