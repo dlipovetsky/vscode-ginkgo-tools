a68927fc10870a79756db8fe25b1fc2a55787972
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2021-02-05 09:47:24 -0800

    highlighter: Implement setSelectionToNodeStart

--- src/highlighter.ts
+++ src/highlighter.ts
@@ -17,15 +17,21 @@ const symbolHighlightDecorationType = vscode.window.createTextEditorDecorationTy
 
 export function highlightNode(editor: vscode.TextEditor, node: outliner.GinkgoNode) {
     const range = rangeFromNode(editor.document, node);
-    editor.revealRange(range, vscode.TextEditorRevealType.InCenterIfOutsideViewport);
     editor.setDecorations(symbolHighlightDecorationType, [range]);
+    editor.revealRange(range, vscode.TextEditorRevealType.InCenterIfOutsideViewport);
 }
 
 export function highlightOff(editor: vscode.TextEditor) {
     editor.setDecorations(symbolHighlightDecorationType, []);
 }
 
-function rangeFromNode(document: vscode.TextDocument, node: outliner. GinkgoNode): vscode.Range {
+export function setSelectionToNodeStart(editor: vscode.TextEditor, node: outliner.GinkgoNode) {
+    const range = rangeFromNode(editor.document, node);
+    const selection = new vscode.Selection(range.start, range.start);
+    editor.selection = selection;
+}
+
+function rangeFromNode(document: vscode.TextDocument, node: outliner.GinkgoNode): vscode.Range {
     const start = document.positionAt(node.start);
     const end = document.positionAt(node.end);
     return new vscode.Range(start, end);
